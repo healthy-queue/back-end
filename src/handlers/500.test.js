bc0f64f096b564facb92243f809c6615544816f8
MuckT <mcgtom10@gmail.com>
2022-03-09 00:16:48 -0800

    test: use long timeout

--- src/handlers/500.test.js
+++ src/handlers/500.test.js
@@ -5,6 +5,7 @@ const supertest = require('supertest')
 const request = supertest(app)
 
 describe('Internal Server Error Handling', () => {
+  jest.setTimeout(10000)
   it("should return 500 on /bad", async () => {
     const response = await request.get('/bad')
     expect(response.status).toBe(500)
