dd0eaca20c1b38bf4dd2b13284304a56e70f0c90
MuckT <mcgtom10@gmail.com>
2022-03-11 02:09:33 -0800

    test: nit 404 handler test changes

--- src/handlers/404.test.js
+++ src/handlers/404.test.js
@@ -5,9 +5,9 @@ const supertest = require('supertest')
 const request = supertest(app)
 
 describe('Not Found', () => {
-  it('404 response status on a bad route', async () => {
-    const response = await request.get('/foo')
     expect(response.status).toBe(404)
+  it('404 response status served on a invalid route', async () => {
+    const response = await request.get('/iDoNotExist')
   })
   it('404 response status served on a invalid HTTP method', async () => {
     const response = await request.patch('/welcome')
