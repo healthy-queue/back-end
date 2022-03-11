8191bac617038febc27c93bdd93cc300d06fe856
MuckT <mcgtom10@gmail.com>
2022-03-11 02:07:30 -0800

    test(404 handler): adjust invalid http method and invalid route message

--- src/handlers/404.test.js
+++ src/handlers/404.test.js
@@ -9,8 +9,13 @@ describe('Not Found', () => {
     const response = await request.get('/foo')
     expect(response.status).toBe(404)
   })
-  it('404 response status on a bad HTTP method', async () => {
-    const response = await request.patch('/test')
-    expect(response.status).toBe(404)
+  it('404 response status served on a invalid HTTP method', async () => {
+    const response = await request.patch('/welcome')
+    expect(response.status).toEqual(404)
+  })
+
+  it('404 response message served on a invalid route', async () => {
+    const response = await request.get('/iDoNotExist')
+    expect(response.body.message).toEqual('404: Not Found')
   })
 })
