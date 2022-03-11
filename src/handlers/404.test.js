68701f700dcb91c32e5d42b941bdd0a15f5d7970
MuckT <mcgtom10@gmail.com>
2022-03-11 02:32:17 -0800

    test: add missing expect

--- src/handlers/404.test.js
+++ src/handlers/404.test.js
@@ -5,9 +5,9 @@ const supertest = require('supertest')
 const request = supertest(app)
 
 describe('Not Found', () => {
-    expect(response.status).toBe(404)
   it('404 response status served on a invalid route', async () => {
     const response = await request.get('/iDoNotExist')
+    expect(response.status).toBe(404)
   })
   it('404 response status served on a invalid HTTP method', async () => {
     const response = await request.patch('/welcome')
