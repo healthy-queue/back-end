45827d9374edc16289944db2c2819ab2f2e42762
MuckT <mcgtom10@gmail.com>
2022-03-11 02:04:11 -0800

    feat: serve only error message in production

--- src/handlers/500.js
+++ src/handlers/500.js
@@ -3,5 +3,7 @@
 module.exports = (err, req, res, next) => {
   if (res.headersSent) return next(err)
   res.status(500)
-  res.json({ error: err })
+  process.env.NODE_ENV === 'production' 
+    ? res.json({ error: err.message })
+    : res.json({ error: err })
 }
