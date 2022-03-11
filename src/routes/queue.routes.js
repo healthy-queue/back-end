a00710b83100887ecd5a17ae20190f22f266c3c5
Ayrat <gimranov45@gmail.com>
2022-03-11 00:55:52 -0500

    deleted commented out code

--- src/routes/queue.routes.js
+++ src/routes/queue.routes.js
@@ -11,7 +11,6 @@ const healthyQueue = {
   red, yellow, green
 }
 
-
 queue_routes.get('/queue', async (req, res) => {
   res.status(200).send([healthyQueue])
 })
@@ -27,57 +26,10 @@ queue_routes.post('/queue/dequeue', async (req, res) => {
 
   let queue = req.body.queue
   let dequeuedPatient = healthyQueue[queue].dequeue()
-
   console.log(dequeuedPatient)
-
   // here add dequeued patient to an ER board (in database)
   // let acceptedPatient = erBoard.create(dequeuedPatient)
-
   res.status(200).send(healthyQueue)
 })
-
-
-// queue_routes.get('/queue/all', async (req,res)=>{
-//   const { range } = queue(redis)
-//   try{
-//     const all_res = await Promise.all([range('red'),range('yellow'),range('green')]).then(res => res).catch(e => e)
-//     res.status(200).send({all_res})
-//   }catch(e){
-//     res.status(500).send({ err:e })
-//   }
-// })
-// queue_routes.get('/queue/which', async (req,res)=>{
-//   const { priority } = req.query
-//   const { range } = queue(redis)
-//   try{
-//     const range_res = await Promise.resolve(range(priority)).then(res => res)
-//     res.status(200).send({range_res})
-//   }catch(e){
-//     const range_rej = await Promise.resolve(range(priority)).then(res => res)
-//     res.status(500).send({err:e,range_rej})
-//   }
-// })
-// queue_routes.post('/queue/push', async (req,res)=>{
-//   const { q_name,patient } = req.body
-//   const { push } = queue(redis)
-//   try{
-//     const push_res = await Promise.resolve(push(q_name,patient)).then(res => res)
-//     res.status(202).send({push_res})
-//   }catch(e){
-//     const push_rej = await Promise.reject(push(q_name,patient)).then(res => res)
-//     res.status(500).send({err:e,push_rej})
-//   }
-// })
-// queue_routes.post('/queue/pop', async (req,res)=>{
-//   const { pop } = queue(redis)
-//   const { q_name } = req.body
-//   try{
-//     const pop_res = await Promise.resolve(pop(q_name)).then(res => res)
-//     res.status(202).send({pop_res})
-//   }catch(e){
-//     const pop_rej = await Promise.reject(pop(q_name)).then(res => res)
-//     res.status(500).send({err:e,pop_rej})
-//   }
-// })
 
 module.exports = queue_routes
